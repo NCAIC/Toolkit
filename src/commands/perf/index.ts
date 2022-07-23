9d3a1cd584fb1216341e197c38f951b4c873fff2
JacobLinCool <jacoblincool@gmail.com>
2022-07-24 02:30:41 +0800

    refactor: run executables with `run_exe`

--- src/commands/perf/index.ts
+++ src/commands/perf/index.ts
@@ -3,11 +3,11 @@ import fs from "node:fs";
 import { execSync } from "node:child_process";
 import { OptionValues, Command } from "commander";
 import { compile } from "../../compile";
-import { runner } from "../../runner";
+import { run_exe } from "../../runner";
 
 const compiled = new Map<string, string>();
 
-export default function check(opts: OptionValues, cmd: Command) {
+export default async function check(opts: OptionValues, cmd: Command) {
     console.log("Running Performance Tests ...");
 
     const dir = path.resolve(__dirname, "..", "programs");
@@ -26,7 +26,7 @@ export default function check(opts: OptionValues, cmd: Command) {
         try {
             let total = 0;
             for (let i = 0; i < 5; i++) {
-                const result = checker();
+                const result = await checker();
                 if (i >= 2) {
                     total += result;
                 }
@@ -43,7 +43,7 @@ export default function check(opts: OptionValues, cmd: Command) {
 }
 
 function test(source: string) {
-    return () => {
+    return async () => {
         if (!compiled.has(source)) {
             const exe = compile(source);
             compiled.set(source, exe);
@@ -53,20 +53,12 @@ function test(source: string) {
 
         if (exe) {
             const dir = path.dirname(exe);
-            const cmd = runner(exe);
-            const time = run(cmd, dir);
+            const run = run_exe(exe, { cwd: dir, stdio: "ignore" });
+            const { time } = await run();
 
             return time;
         } else {
             throw new Error("Compilation failed");
         }
     };
-}
-
-function run(command: string, cwd: string): number {
-    const start_t = Date.now();
-    execSync(command, { cwd, stdio: "ignore" });
-    const end_t = Date.now();
-
-    return end_t - start_t;
 }
